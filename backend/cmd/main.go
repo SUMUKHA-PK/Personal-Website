8ccd92026e72a303ba82a18e6f30d4c3e385b53a
SUMUKHA-PK <sumukhapk46@gmail.com>
2020-05-30 11:37:31 +0530

    update

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/SUMUKHA-PK/Basic-Golang-Server/server"
-	"github.com/SUMUKHA-PK/Personal-Website/backend/routing"
+	"github.com/SUMUKHA-PK/Personal-Website/backend/internal/routing"
 	"github.com/gorilla/mux"
 )
 
